d7ac56908a1cde746ff0d5dc44481d2f3519ea2e
DongChuelKim <terrinens@gmail.com>
2024-10-17 20:05:32 +0900

    fix : yml 실행구문 변경

--- src/app/lib/Posts.ts
+++ src/app/lib/Posts.ts
@@ -4,9 +4,7 @@ import default_img from '@/public/post_default.jpg'
 import {compileMDX} from "next-mdx-remote/rsc";
 import {PostCardProps} from "@/app/components/PostRender";
 
-const postsDir = process.env.NODE_ENV === 'production'
-    ? path.join(process.cwd(), '/.next/server/posts')
-    : path.join(process.cwd(), '/src/posts');
+const postsDir = path.join(process.cwd(), '/src/posts');
 
 export class Paging {
     size: number;
