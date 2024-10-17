9d5862b7fbece05611c56f19bd2cb849cf111202
DongChuelKim <terrinens@gmail.com>
2024-10-17 20:44:09 +0900

    fix : yml 실행구문 변경 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,21 @@
 import createdMDX from '@next/mdx'
 
-const prefix = process.env.NODE_ENV === 'production' ? 'https://github.com/terrinens/blog' : '';
+const prefix = process.env.NODE_ENV === 'production' ? '/blog' : '';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     assetPrefix: prefix,
+    /* basePath 문제? build 할때 파일을 가져오기를 기대하는것이 아닌 reop에서 직접 가져오게 시도. */
+    basePath: prefix,
     trailingSlash: true,
     output: 'export',
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
     distDir: 'build',
+
+    images: {
+        unoptimized: true,
+    },
 };
 
 const withMDX = createdMDX({})
