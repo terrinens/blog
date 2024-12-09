3ff864c862b9279b3d1c13af4551d136bd469d27
DongChuelKim <terrinens@gmail.com>
2024-12-09 16:31:07 +0900

    feat : 사이트맵 노출 추가

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+const {withSitemap} = require('next-sitemap');
+
+module.exports = withSitemap({
     siteUrl: 'https://terrinens.github.io/blog/',
     generateRobotsTxt: true,
-}
+    robotsTxtOptions: {
+        policies: [
+            {userAgent: '*', allow: '/'},
+        ],
+    }
+})
