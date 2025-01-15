6228f25aede63066821ec27f70d6a3fe760555f6
DongChuelKim <terrinens@gmail.com>
2025-01-16 08:53:58 +0900

    fix : http listen 수정

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM asia-northeast2-docker.pkg.dev/github-blog-b7f62/cloud-run-source-deploy/blog/blog
+
+CMD go run main.go
--- main.go
+++ main.go
@@ -16,11 +16,10 @@ func main() {
 	db.InitClient()
 	InitRouter()
 
-	if len(os.Args) >= 1 && os.Args[1] == "dev" {
-		log.Println("Server Listening Port:", port)
-		if err := http.ListenAndServe(":"+port, nil); err != nil {
-			log.Fatalf("Error starting server: %v", err)
-		}
+	log.Println("Server starting on port " + port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %s", err)
 	}
 }
 
@@ -28,12 +27,5 @@ func init() {
 	err := os.Setenv("GOOGLE_PROJECT_ID", "github-blog-b7f62")
 	if err != nil {
 		log.Fatal("Failed to set FIRESTORE_PROJECT_ID environment variable")
-	}
-
-	if len(os.Args) > 1 && os.Args[1] == "dev" {
-		err := os.Setenv("DEV", "true")
-		if err != nil {
-			log.Fatal("Failed to set DEV environment variable")
-		}
 	}
 }
